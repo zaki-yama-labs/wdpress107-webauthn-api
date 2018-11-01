42f493d7bb20c9b4d2090cedf13f3e6cd7ddee7b
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-02 02:39:47 +0900

    公開鍵生成リクエスト

--- util.js
+++ util.js
@@ -20,3 +20,63 @@ function buf2str(buf) {
 // チャレンジ値の生成
 let challengeBuf = new Uint8Array(16);
 window.crypto.getRandomValues(challengeBuf);
+
+
+// 公開鍵生成リクエストのパラメータ
+const publicKey = {
+  challenge: challengeBuf,
+  rp: {
+    name: 'Example',
+    id: 'example.com',
+  },
+  user: {
+    id: str2buf('ExampleId'),
+    name: 'example@example.com',
+    displayName: 'Example Name',
+  },
+  pubKeyCredParams: [{
+    alg: -7,
+    type: 'public-key',
+  }],
+  authenticatorSelection: {
+    authenticatorAttachment: 'cross-platfrom',
+    requireResidentKey: false,
+    userVerification: 'preferred',
+  },
+};
+
+// 公開鍵生成リクエスト
+navigator.credentials.create({ publicKey })
+  .then(function(attestation) {
+    // 公開鍵の作成に成功
+    console.dir(attestation);
+    /**
+    // attestation に含まれるデータ
+    attestation = {
+      rawId,
+      id,
+      type,
+      response: {
+        clientDataJSON,
+        attestationObject,
+      }
+    };
+    */
+    console.dir(JSON.parse(
+      buf2str(attestation.response.clientDataJSON)
+    ));
+    /**
+    // attestation.response.clientDataJSONに含まれるデータ
+    clientDataJSON = {
+      challenge,
+      clientExtensions,
+      hashAlgorithm,
+      origin,
+      type
+    }
+    */
+  })
+  .catch(function(error) {
+    // 公開鍵の作成に失敗
+  });
+
